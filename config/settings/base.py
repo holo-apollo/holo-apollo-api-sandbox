9eafdac64c72452c974aca3fe7c919ff5eabc997
Valerii Kovalchuk <kovvalole@gmail.com>
2018-02-07 09:04:32 +0200

    Refactor to settings directory

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,8 +15,7 @@ import sys
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
