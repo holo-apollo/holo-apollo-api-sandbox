7159b95cdc2fe73092b4cd0124d178a859aaa4b3
Valerii Kovalchuk <kovvalole@gmail.com>
2018-02-12 15:26:05 +0200

    Config pipeline

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,3 +1,9 @@
 from .base import *
 
 CELERY_ALWAYS_EAGER = True
+
+TEST_DATABASES = DATABASES
+TEST_DATABASE_URL = dotenv.get('TEST_DATABASE_URL')
+if TEST_DATABASE_URL:
+    TEST_DATABASES['default'] = dj_database_url.config(env='TEST_DATABASE_URL')
+    TEST_RUNNER = 'config.test_runner.HerokuTestSuiteRunner'
---
+++ config/test_runner.py
@@ -0,0 +1,59 @@
+import django
+from django.conf import settings
+from django.core.management import call_command
+from django.db.utils import ConnectionHandler
+from django.test.runner import DiscoverRunner
+
+
+class HerokuTestSuiteRunner(DiscoverRunner):
+    def setup_databases(self, **kwargs):
+        ###
+        # WARNING: NOT handling 'TEST_MIRROR', 'TEST_DEPENDENCIES'
+        ###
+
+        # get new connections to test database
+        test_connections = ConnectionHandler(settings.TEST_DATABASES)
+
+        for alias in django.db.connections:
+            test_connection = test_connections[alias]
+
+            # set django-wide connection to use test connection
+            django.db.connections[alias] = test_connection
+
+            # re-initialize database (this "replaces" the CREATE DATABASE which
+            # cannot be issued on Heroku)
+            cursor = test_connection.cursor()
+            cursor.execute('DROP SCHEMA public CASCADE')
+            cursor.execute('CREATE SCHEMA public')
+
+            # code below taken from
+            # django.test.simple.DjangoTestSuiteRunner.setup_databases and
+            # django.db.backends.creation.create_test_db
+
+            # make them tables
+            call_command(
+                'syncdb',
+                verbosity=0,
+                interactive=False,
+                database=test_connection.alias,
+                load_initial_data=False
+            )
+
+            call_command(
+                'flush',
+                verbosity=0,
+                interactive=False,
+                database=test_connection.alias
+            )
+
+            from django.core.cache import caches
+            from django.core.cache.backends.db import BaseDatabaseCache
+            for cache_alias in settings.CACHES:
+                cache = caches(cache_alias)
+                if isinstance(cache, BaseDatabaseCache):
+                    call_command('createcachetable', cache._table,
+                                 database=test_connection.alias)
+
+    def teardown_databases(self, *args, **kwargs):
+        # NOP
+        pass
