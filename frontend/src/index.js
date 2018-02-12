9ba8dada572154af6d281e8d7440132b32e045ce
Valerii Kovalchuk <kovvalole@gmail.com>
2018-02-13 00:14:23 +0200

    #17 Use React for landing page rendering

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Landing from 'apps/landing/containers/landing';
+
+
+const $root = document.getElementById('root');
+
+ReactDOM.render(<Landing/>, $root);
