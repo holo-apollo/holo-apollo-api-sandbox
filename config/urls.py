cd9ad44903782835d76a2c43f5a6429940f9133e
Valerii Kovalchuk <kovvalole@gmail.com>
2018-02-24 17:40:58 +0200

    #36 Bring back URL for now

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('logout/', auth_views.logout, name='logout'),
     path('password_reset/', auth_views.password_reset, name='password_reset'),
+    path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             auth_views.password_reset_confirm, name='password_reset_confirm'),
     path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
