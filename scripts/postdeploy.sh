f15608e8a9c073f036cc1cbd20f72160f187a1d9
Valerii Kovalchuk <kovvalole@gmail.com>
2018-08-27 22:30:48 +0300

    Fix loading dump to review apps 3

--- scripts/postdeploy.sh
+++ scripts/postdeploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-psql $DATABASE_URL -f copy_dump.sql
+psql $DATABASE_URL -f scripts/copy_dump.sql
 pg_dump $STAGING_DATABASE_URL | psql $DATABASE_URL
 python manage.py migrate --noinput
