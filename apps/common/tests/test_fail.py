877aa4656a78f63b1fc2dae71ac372ba8dad3ea6
Valerii Kovalchuk <kovvalole@gmail.com>
2018-08-28 10:58:52 +0300

    Remove false tests

--- apps/common/tests/test_fail.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-
-class TestFailing(TestCase):
-    def test_failing(self):
-        self.assertEqual(2 + 2, 5)
-
-    def test_failing_2(self):
-        self.assertTrue(False)
