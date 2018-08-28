06c565f355c532e63742dda5de707a01ecd4ee70
Valerii Kovalchuk <kovvalole@gmail.com>
2018-08-28 10:35:22 +0300

    Check several fails

--- apps/common/tests/test_fail.py
+++ apps/common/tests/test_fail.py
@@ -4,3 +4,6 @@ from django.test import TestCase
 class TestFailing(TestCase):
     def test_failing(self):
         self.assertEqual(2 + 2, 5)
+
+    def test_failing_2(self):
+        self.assertTrue(False)
---
+++ frontend/test/test_components/test_failing.js
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+
+describe('Failing test', function() {
+    it('should fail', function() {
+        expect(false).to.equal(true);
+    });
+});
