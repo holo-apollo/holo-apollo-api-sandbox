d1826f6a48a4968b533910e92fe5f6c69fb74244
Valerii Kovalchuk <kovvalole@gmail.com>
2018-09-09 10:39:04 +0300

    Add flake8 check

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+flake8 apps/
+ret_code_flake=$?
+
 python manage.py test
 ret_code_django=$?
 
 npm test
 ret_code_npm=$?
 
-if [ $ret_code_django != 0 ] || [ $ret_code_npm != 0 ] ; then
+if [ $ret_code_flake != 0 ] || [ $ret_code_django != 0 ] || [ $ret_code_npm != 0 ] ; then
   exit 1
 fi
