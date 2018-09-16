063d4eb635321d7e94418be8b143fa4fcab8150d
Valerii Kovalchuk <kovvalole@gmail.com>
2018-09-16 13:32:05 +0300

    Fix flake8 errors

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -11,7 +11,7 @@ class HoloUserAdmin(UserAdmin):
     list_display = ('email', 'username', 'phone', 'get_full_name')
 
     fieldsets = (
-        (None, {'fields': ('username', 'password','email_confirmed', 'email_confirm_token')}),
+        (None, {'fields': ('username', 'password', 'email_confirmed', 'email_confirm_token')}),
         (_('Personal info'), {'fields': ('email', 'phone', 'first_name', 'first_name_ru',
                                          'first_name_uk', 'first_name_en', 'last_name',
                                          'last_name_ru', 'last_name_uk', 'last_name_en',
