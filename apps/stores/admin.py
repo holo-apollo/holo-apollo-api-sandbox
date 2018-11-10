048890dfb27c97bf83c65f264ab0cd172c5dbf29
Valerii Kovalchuk <kovvalole@gmail.com>
2018-11-10 14:21:24 +0200

    #73 Model and admin

--- apps/stores/admin.py
+++ apps/stores/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 from common.admin import Select2ModelAdmin
-from .models import Store
+from .models import Store, StoreApplication
 
 
 @admin.register(Store)
 class StoreAdmin(Select2ModelAdmin):
     pass
+
+
+@admin.register(StoreApplication)
+class StoreApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'instagram_name', 'category',)
--- apps/stores/models.py
+++ apps/stores/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from model_utils.models import TimeStampedModel
 
@@ -12,3 +14,63 @@ class Store(TimeStampedModel):
 
     def __str__(self):
         return f'Store: {self.user}'
+
+
+class StoreApplication(TimeStampedModel):
+    CLOTHES = 'clothes'
+    JEWELRY = 'jewelry'
+    ACCESSORIES = 'accessories'
+    HOME_DECOR = 'home_decor'
+    SHOES = 'shoes'
+
+    CATEGORY_CHOICES = (
+        (CLOTHES, _('Clothes')),
+        (JEWELRY, _('Jewelry')),
+        (ACCESSORIES, _('Accessories')),
+        (HOME_DECOR, _('Home decor')),
+        (SHOES, _('Shoes')),
+    )
+
+    name = models.CharField(verbose_name=_('Name'), max_length=61)
+    email = models.EmailField(
+        verbose_name=_('Email'),
+        max_length=254,
+        unique=True,
+        error_messages={
+            'unique': _('That email address is already used.')
+        }
+    )
+    instagram_name = models.CharField(
+        verbose_name=_('Instagram name'),
+        max_length=254,
+        unique=True,
+        error_messages={
+            'unique': _('That Instagram name is already used.')
+        }
+    )
+    category = models.CharField(
+        verbose_name=_('Category'),
+        max_length=30,
+        choices=CATEGORY_CHOICES,
+    )
+    selling_goods = models.TextField(
+        verbose_name=_('Selling goods'),
+        max_length=500
+    )
+    goods_description = models.TextField(
+        verbose_name=_('Goods description'),
+        max_length=1000,
+        validators=[MinLengthValidator(500)]
+    )
+    philosophy = models.TextField(
+        verbose_name=_('Philosophy'),
+        max_length=1000,
+        validators=[MinLengthValidator(500)]
+    )
+    data_usage_agreement = models.BooleanField(
+        verbose_name=_('Data usage agreement'),
+        default=False
+    )
+
+    def __str__(self):
+        return f'Application: {self.email} {self.instagram_name}'
