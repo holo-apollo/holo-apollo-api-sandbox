ee66799061fff206a78680a7eb298c022b065dc4
Valerii Kovalchuk <kovvalole@gmail.com>
2018-11-11 22:03:05 +0200

    Fix flake8

--- apps/stores/api/v1/urls.py
+++ apps/stores/api/v1/urls.py
@@ -2,6 +2,5 @@ from rest_framework import routers
 
 from .views import StoreApplicationViewSet
 
-
 router = routers.SimpleRouter()
 router.register(r'stores/applications', StoreApplicationViewSet)
