c148447f911d4c2f3e305252248d29a7dd99f24d
Valerii Kovalchuk <kovvalole@gmail.com>
2018-11-17 19:41:46 +0200

    Add eslint check to test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,6 +9,12 @@ ret_code_django=$?
 npm test
 ret_code_npm=$?
 
-if [ $ret_code_flake != 0 ] || [ $ret_code_django != 0 ] || [ $ret_code_npm != 0 ] ; then
+npm run flow
+ret_code_flow=$?
+
+npm run eslint
+ret_code_eslint=$?
+
+if [ $ret_code_flake != 0 ] || [ $ret_code_django != 0 ] || [ $ret_code_npm != 0 ] || [ $ret_code_flow != 0 ] || [ $ret_code_eslint != 0 ] ; then
   exit 1
 fi
