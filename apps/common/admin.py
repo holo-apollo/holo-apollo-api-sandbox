ae6f38e92bf76cb05683e4f8a89e203d0d55f805
Valerii Kovalchuk <kovvalole@gmail.com>
2018-11-25 13:51:19 +0200

    Fix flake8

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from rest_framework.authtoken.models import Token
 from social_django.models import Association, Nonce, UserSocialAuth
 
-
 if not settings.DEBUG:
     admin.site.unregister(Association)
     admin.site.unregister(Nonce)
