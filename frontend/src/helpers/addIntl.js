844e22d034a629830909916c63f89c4322a1d7c7
Valerii Kovalchuk <kovvalole@gmail.com>
2019-02-03 11:08:51 +0200

    Add translations for application form

--- frontend/src/helpers/addIntl.js
+++ frontend/src/helpers/addIntl.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { IntlProvider, injectIntl } from 'react-intl';
+import { IntlProvider, addLocaleData, injectIntl } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import ru from 'react-intl/locale-data/ru';
+import uk from 'react-intl/locale-data/uk';
+
+import localeData from '../../i18n/locale/data.json';
+
+const lang = document.documentElement.lang;
+const messages = localeData[lang] || localeData.en;
 
 const addIntl = Component => {
+  switch (lang) {
+    case 'ru':
+      addLocaleData(ru);
+      break;
+    case 'uk':
+      addLocaleData(uk);
+      break;
+    default:
+      addLocaleData(en);
+  }
+
   const ComponentWithIntl = injectIntl(Component);
   return () => (
-    <IntlProvider locale={document.documentElement.lang}>
+    <IntlProvider locale={document.documentElement.lang} messages={messages}>
       <ComponentWithIntl />
     </IntlProvider>
   );
