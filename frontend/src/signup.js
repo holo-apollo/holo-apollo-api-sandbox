7dc2111f4cec6c5425e7ffc22d3fafe8417e7089
Valerii Kovalchuk <kovvalole@gmail.com>
2019-02-05 21:10:45 +0200

    Cleanup

--- frontend/src/signup.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './axios_defaults';
-import Signup from 'apps/users/containers/signup';
-
-ReactDOM.render(<Signup />, document.getElementById('react-signup'));
