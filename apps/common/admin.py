7b58d878001efced975fcd23e795eb9253ca43a2
Valerii Kovalchuk <kovvalole@gmail.com>
2019-02-06 09:49:11 +0200

    Replace ugettext with gettext

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from rest_framework.authtoken.models import Token
 from social_django.models import Association, Nonce, UserSocialAuth
