03939b99bcbe37fa80912286d9e21aa34c593f09
Valerii Kovalchuk <kovvalole@gmail.com>
2019-02-20 15:27:42 +0200

    Try to fix regex

--- config/settings/development.py
+++ config/settings/development.py
@@ -7,8 +7,5 @@ STATICFILES_LOCATION = 'static'
 MEDIAFILES_LOCATION = 'media-development'
 
 CORS_ORIGIN_REGEX_WHITELIST = [
-    r'localhost:\d+',
-    r'127\.0\.0\.1:\d+',
-    r'holo-apollo-ui-test\.herokuapp\.com',
-    r'holo-apollo-ui-test-pr-\d+\.herokuapp\.com'
+    r'^(https?://)?(\w+\.)?holo-apollo-ui-test(-pr-\d+)?\.herokuapp\.com$',
 ]
