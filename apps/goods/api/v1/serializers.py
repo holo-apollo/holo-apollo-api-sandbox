568e873c35482e7f7b5ac9ad280705434bdf4101
Valerii Kovalchuk <kovvalole@gmail.com>
2019-03-02 00:03:27 +0200

    Add specifications to good API

--- apps/goods/api/v1/serializers.py
+++ apps/goods/api/v1/serializers.py
@@ -1,8 +1,17 @@
 from djmoney.money import Money
 from rest_framework import serializers
 
-from goods.models import Good
+from goods.models import Good, GoodSpecifications
 from stores.api.v1.serializers import SimpleStoreSerializer
+
+
+class GoodSpecificationsSerializer(serializers.ModelSerializer):
+    color = serializers.ReadOnlyField(source='color.definition')
+    size = serializers.ReadOnlyField(source='size.definition')
+
+    class Meta:
+        model = GoodSpecifications
+        fields = ['color', 'size']
 
 
 class GoodSerializer(serializers.ModelSerializer):
@@ -11,11 +20,12 @@ class GoodSerializer(serializers.ModelSerializer):
     price = serializers.FloatField(source='price.amount')
     price_currency = serializers.CharField(source='price.currency', default='UAH')
     seller_info = SimpleStoreSerializer(read_only=True, source='seller')
+    specifications = GoodSpecificationsSerializer(read_only=True)
 
     class Meta:
         model = Good
         fields = ['id', 'name', 'description', 'category', 'price', 'price_currency',
-                  'categories_ids', 'user', 'seller_info']
+                  'categories_ids', 'user', 'seller_info', 'specifications']
 
     def validate(self, validated_data):
         user = validated_data.pop('user')
